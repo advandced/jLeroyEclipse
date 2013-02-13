2c02bfef82815008fee77be660fa27d91228900f
Gregoire <Gregoire@192.168.169.130>
2013-02-13 14:52:18 +0100

    Detection des urls à restreindre dans un list

--- jProductBaseWeb/src/fr/la/juserright/managedbean/LoginPageFilter.java
+++ jProductBaseWeb/src/fr/la/juserright/managedbean/LoginPageFilter.java
@@ -1,6 +1,8 @@
 package fr.la.juserright.managedbean;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -17,27 +19,37 @@ import fr.la.jproductbaseweb.beanmanaged.LoginBean;
 @WebFilter(urlPatterns = { "/panel.jsf", "/param/*", "/entryPROD/*",
 		"/entrySAV/*", "/admin/*" })
 public class LoginPageFilter implements Filter {
+	
+	List<String> permList = new ArrayList<String>();
+	boolean urlFound = false;
+	
+	public LoginPageFilter(){
+		permList.add("/jProductBaseWeb/param/productConfModel.jsf");
+		permList.add("/jProductBaseWeb/param/productConf.jsf");
+	}
 
 	public void init(FilterConfig config) throws ServletException {
 		// TODO Auto-generated method stub
-
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws ServletException, IOException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		LoginBean auth = (LoginBean) req.getSession().getAttribute("loginBean");
+		
+		//System.out.println("-->"+permList+"<--");
+		
+		// retourne l'addresse complète du fichier
+		String fullURI = req.getRequestURI();
+		
+		urlFound = permList.contains(fullURI);
 
 		if (auth != null && auth.isUserconnected()) {
-			// retourne l'addresse complète du fichier
-			String fullURI = req.getRequestURI();
-
-			if (fullURI.matches("/jProductBaseWeb/param/productConfModel.jsf")) {
-				// System.out.println(fullURI);
+			if (urlFound) {
 				HttpServletResponse res = (HttpServletResponse) response;
 				res.sendRedirect(req.getContextPath() + "/error403.jsf");
 				chain.doFilter(request, response);
-			} else {
+			}  else {
 				// L'utilisateur est connecte on le laisse poursuivre sa requete
 				chain.doFilter(request, response);
 			}
